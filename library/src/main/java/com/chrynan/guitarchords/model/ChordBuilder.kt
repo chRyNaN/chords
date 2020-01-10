23618bf516016269ec68474fbacc80832ddcea63
Chris <ckeenan@starry.com>
2020-01-09 19:47:12 -0500

    Created ChordBuilder

---
+++ library/src/main/java/com/chrynan/guitarchords/model/ChordBuilder.kt
@@ -0,0 +1,21 @@
+package com.chrynan.guitarchords.model
+
+class ChordBuilder(private val name: String? = null) {
+
+    private val markers = mutableSetOf<ChordMarker>()
+
+    operator fun ChordMarker.unaryPlus() {
+        markers.add(this)
+    }
+
+    internal fun build(): Chord = Chord(
+            name = name,
+            markers = markers)
+}
+
+
+fun chord(name: String? = null, builder: ChordBuilder.() -> Unit): Chord {
+    val chordBuilder = ChordBuilder(name = name)
+    builder.invoke(chordBuilder)
+    return chordBuilder.build()
+}
