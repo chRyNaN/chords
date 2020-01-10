d2f7300486d11553f0fb13ff6b05d684d52e200d
Chris <ckeenan@starry.com>
2020-01-09 23:05:13 -0500

    Converted TouchableSpan and LinkTouchMovementMethod to Kotlin and refactored the classes

---
+++ library/src/main/java/com/chrynan/guitarchords/span/LinkTouchMovementMethod.kt
@@ -0,0 +1,93 @@
+package com.chrynan.guitarchords.span
+
+import android.text.Selection
+import android.text.Spannable
+import android.text.method.LinkMovementMethod
+import android.view.MotionEvent
+import android.widget.TextView
+
+/*
+ * Copyright 2016 chRyNaN
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * References: http://stackoverflow.com/a/7292485/1478764,
+ * http://stackoverflow.com/a/20905824/1478764
+ */
+class LinkTouchMovementMethod : LinkMovementMethod() {
+
+    private var pressedSpan: TouchableSpan? = null
+
+    override fun onTouchEvent(widget: TextView, buffer: Spannable, event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                pressedSpan = getPressedSpan(widget, buffer, event)
+
+                if (pressedSpan != null) {
+                    pressedSpan!!.isPressed = true
+                    Selection.setSelection(buffer, buffer.getSpanStart(pressedSpan),
+                            buffer.getSpanEnd(pressedSpan))
+                    pressedSpan!!.onTouch(widget, event)
+                }
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val touchedSpan = getPressedSpan(widget, buffer, event)
+
+                if (pressedSpan != null && touchedSpan !== pressedSpan) {
+                    pressedSpan!!.isPressed = false
+                    pressedSpan = null
+                    Selection.removeSelection(buffer)
+                } else if (pressedSpan != null) {
+                    pressedSpan!!.onTouch(widget, event)
+                }
+            }
+            else -> {
+                if (pressedSpan != null) {
+                    pressedSpan!!.onTouch(widget, event)
+                    pressedSpan!!.isPressed = false
+                    super.onTouchEvent(widget, buffer, event)
+                }
+
+                pressedSpan = null
+
+                Selection.removeSelection(buffer)
+            }
+        }
+
+        return true
+    }
+
+    private fun getPressedSpan(textView: TextView, spannable: Spannable, event: MotionEvent): TouchableSpan? {
+        var x = event.x.toInt()
+        var y = event.y.toInt()
+
+        x -= textView.totalPaddingLeft
+        y -= textView.totalPaddingTop
+        x += textView.scrollX
+        y += textView.scrollY
+
+        val layout = textView.layout
+        val line = layout.getLineForVertical(y)
+        val off = layout.getOffsetForHorizontal(line, x.toFloat())
+        val link = spannable.getSpans(off, off, TouchableSpan::class.java)
+        var touchedSpan: TouchableSpan? = null
+
+        if (link.isNotEmpty()) {
+            touchedSpan = link[0]
+        }
+
+        return touchedSpan
+    }
+}
---
+++ library/src/main/java/com/chrynan/guitarchords/span/TouchableSpan.kt
@@ -0,0 +1,60 @@
+package com.chrynan.guitarchords.span
+
+import android.graphics.Color
+import android.text.TextPaint
+import android.text.style.CharacterStyle
+import android.text.style.UpdateAppearance
+import android.view.MotionEvent
+import android.view.View
+
+/*
+ * Copyright 2016 chRyNaN
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * References: http://stackoverflow.com/a/7292485/1478764,
+ * http://stackoverflow.com/a/20905824/1478764
+ *
+ * If an object of this type is attached to the text of a TextView
+ * with a movement method of LinkTouchMovementMethod, the affected spans of
+ * text can be selected.  If touched, the [.onTouch] method will
+ * be called.
+ */
+abstract class TouchableSpan : CharacterStyle(), UpdateAppearance {
+
+    var isPressed = false
+    var backgroundColor = Color.TRANSPARENT
+    var pressedBackgroundColor = Color.TRANSPARENT
+    var textColor = Color.BLUE
+    var pressedTextColor = Color.BLUE
+
+    /**
+     * Performs the touch action associated with this span.
+     *
+     * @return
+     */
+    abstract fun onTouch(widget: View?, m: MotionEvent?): Boolean
+
+    /**
+     * Could make the text underlined or change link color.
+     */
+    override fun updateDrawState(textPaint: TextPaint) {
+        textPaint.apply {
+            color = if (isPressed) pressedTextColor else textColor
+            bgColor = if (isPressed) pressedBackgroundColor else backgroundColor
+            isUnderlineText = false
+        }
+    }
+}
