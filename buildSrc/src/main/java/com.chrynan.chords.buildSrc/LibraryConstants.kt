8fad57f7fb71bb18d87e0bee8f1946a8647d45c1
Chris <ckeenan@starry.com>
2020-10-10 13:48:59 -0400

    Added buildSrc

---
+++ buildSrc/src/main/java/com.chrynan.chords.buildSrc/LibraryConstants.kt
@@ -0,0 +1,17 @@
+package com.chrynan.chords.buildSrc
+
+object LibraryConstants {
+
+    const val bintrayRepo = "chrynan"
+    const val group = "com.chrynan.chords"
+    const val owner = "chrynan"
+    const val repoName = "chords"
+    const val versionName = "2.3.1"
+    const val versionCode = 4
+    const val versionDescription = "Release 2.3.1"
+    const val license = "Apache-2.0"
+    const val vcsUrl = "https://github.com/chRyNaN/chords.git"
+    const val androidCompileSdkVersion = 29
+    const val androidMinSdkVersion = 23
+    const val androidTargetSdkVersion = 29
+}
