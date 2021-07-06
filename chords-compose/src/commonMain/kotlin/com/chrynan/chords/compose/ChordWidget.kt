96c30069e27963109c4871b79ecb21f856ce6487
Chris <ckeenan@starry.com>
2021-07-05 22:04:45 -0500

    Created Kotlin Multiplatform chords-compose module

--- chords-compose/src/commonMain/kotlin/com/chrynan/chords/compose/ChordWidget.kt
+++ chords-compose/src/commonMain/kotlin/com/chrynan/chords/compose/ChordWidget.kt
@@ -1,14 +1,12 @@
 @file:Suppress("unused")
 
-package com.chrynan.chords.android.compose
+package com.chrynan.chords.compose
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.AndroidView
 import com.chrynan.chords.model.Chord
 import com.chrynan.chords.model.ChordChart
 import com.chrynan.chords.model.ChordViewModel
-import com.chrynan.chords.view.ChordViewBinder
 
 @ExperimentalUnsignedTypes
 @Composable
@@ -18,16 +16,5 @@ fun ChordWidget(
     chart: ChordChart = ChordChart.STANDARD_TUNING_GUITAR_CHART,
     viewModel: ChordViewModel = ChordViewModel()
 ) {
-    AndroidView(
-        factory = { com.chrynan.chords.widget.ChordWidget(context = it) },
-        modifier = modifier,
-        update = { widget ->
-            val binder = ChordViewBinder(widget)
 
-            widget.chord = chord
-            widget.chart = chart
-
-            binder.bind(viewModel)
-        }
-    )
 }
