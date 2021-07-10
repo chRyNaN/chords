43c0c3790a3696d31c1b983f24dcc75614a08b6c
Chris <ckeenan@starry.com>
2021-07-10 10:09:22 -0500

    New Approach to ComposableCanvas

--- chords-compose/src/commonMain/kotlin/com/chrynan/chords/compose/ComposableCanvas.kt
+++ chords-compose/src/commonMain/kotlin/com/chrynan/chords/compose/ComposableCanvas.kt
@@ -3,118 +3,46 @@
 package com.chrynan.chords.compose
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.DpOffset
 
 interface ComposableDrawScope : DrawScope {
 
-    @Composable
     fun drawComposable(
-        modifier: Modifier = Modifier,
-        size: PositionalLayoutSize,
-        offset: Offset,
         content: @Composable () -> Unit
     )
-}
-
-@Composable
-fun ComposableDrawScope.drawComposable(
-    modifier: Modifier = Modifier,
-    size: Size,
-    offset: Offset,
-    content: @Composable () -> Unit
-) {
-    drawComposable(
-        modifier = modifier,
-        size = PositionalLayoutSize.Exact(size = size),
-        offset = offset,
-        content = content
-    )
-}
-
-@Composable
-fun ComposableDrawScope.drawComposable(
-    modifier: Modifier = Modifier,
-    width: Float,
-    height: Float,
-    offset: Offset,
-    content: @Composable () -> Unit
-) {
-    drawComposable(
-        modifier = modifier,
-        size = Size(width = width, height = height),
-        offset = offset,
-        content = content
-    )
-}
-
-@Composable
-fun ComposableDrawScope.drawComposable(
-    modifier: Modifier = Modifier,
-    width: Dp,
-    height: Dp,
-    offset: DpOffset,
-    content: @Composable () -> Unit
-) {
-    drawComposable(
-        modifier = modifier,
-        size = Size(width = width.toPx(), height = height.toPx()),
-        offset = Offset(x = offset.x.toPx(), y = offset.y.toPx()),
-        content = content
-    )
-}
-
-internal class ComposableDrawScopeSource(
-    private val positionalLayoutScope: PositionalLayoutScope,
-    private val drawScope: DrawScope
-) : ComposableDrawScope,
-    DrawScope by drawScope {
-
-    @Composable
-    override fun drawComposable(
-        modifier: Modifier,
-        size: PositionalLayoutSize,
-        offset: Offset,
-        content: @Composable () -> Unit
-    ) {
-        positionalLayoutScope.Layout(
-            modifier = modifier,
-            size = size,
-            offset = offset,
-            content = content
-        )
-    }
 }
 
 @Composable
 fun ComposableCanvas(
     modifier: Modifier = Modifier,
-    onDraw: @Composable ComposableDrawScope.() -> Unit
+    onDraw: ComposableDrawScope.() -> Unit
 ) {
-    PositionalLayout(modifier = modifier) {
-        val positionalLayoutScope = this
+    Box(modifier = modifier) {
+        val composableOperations = mutableListOf<DrawComposableOperation>()
 
-        Layout(
-            modifier = Modifier,
-            offset = Offset(0f, 0f),
-            size = PositionalLayoutSize.MatchParent
-        ) {
-            Canvas(modifier = Modifier.fillMaxSize()) {
-                val drawScope = this
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            val drawScope = this
 
-                val scope = ComposableDrawScopeSource(
-                    positionalLayoutScope = positionalLayoutScope,
-                    drawScope = drawScope
-                )
+            val scope = object : ComposableDrawScope,
+                DrawScope by drawScope {
 
-                onDraw(scope)
+                override fun drawComposable(content: @Composable () -> Unit) {
+                    composableOperations.add(DrawComposableOperation(content = content))
+                }
             }
+
+            onDraw(scope)
+        }
+
+        composableOperations.forEach {
+            it.content.invoke()
         }
     }
 }
+
+data class DrawComposableOperation(val content: @Composable () -> Unit)
--- chords-compose/src/commonMain/kotlin/com/chrynan/chords/compose/DrawChordWidget.kt
+++ chords-compose/src/commonMain/kotlin/com/chrynan/chords/compose/DrawChordWidget.kt
@@ -1,14 +1,15 @@
 package com.chrynan.chords.compose
 
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import com.chrynan.chords.model.ChordChart
 
-@Composable
 internal fun ComposableDrawScope.drawFretNumbers(
     fretNumberPoints: List<Offset>,
     chart: ChordChart,
@@ -17,43 +18,41 @@ internal fun ComposableDrawScope.drawFretNumbers(
     // Fret numbers; check if we are showing them or not
     if (showFretNumbers) {
         fretNumberPoints.forEachIndexed { index, point ->
-            drawComposable(
-                size = PositionalLayoutSize.WrapContent,
-                offset = Offset(x = point.x, y = point.y)
-            ) {
-                Text(text = (chart.fretStart.number + index).toString())
+            drawComposable {
+                Text(
+                    modifier = Modifier.offset(x = point.x.toDp(), y = point.y.toDp()),
+                    text = (chart.fretStart.number + index).toString()
+                )
             }
         }
     }
 }
 
-@Composable
 internal fun ComposableDrawScope.drawStringMarkers(
     stringTopMarkerPositions: List<StringPosition>,
     stringBottomLabelPositions: List<StringPosition>,
 ) {
     // Top String markers (open/muted)
     stringTopMarkerPositions.forEach {
-        drawComposable(
-            size = PositionalLayoutSize.WrapContent,
-            offset = Offset(x = it.textX, y = it.textY)
-        ) {
-            Text(text = it.text)
+        drawComposable {
+            Text(
+                modifier = Modifier.offset(x = it.textX.toDp(), y = it.textY.toDp()),
+                text = it.text
+            )
         }
     }
 
     // Bottom String labels (number/note)
     stringBottomLabelPositions.forEach {
-        drawComposable(
-            size = PositionalLayoutSize.WrapContent,
-            offset = Offset(x = it.textX, y = it.textY)
-        ) {
-            Text(text = it.text)
+        drawComposable {
+            Text(
+                modifier = Modifier.offset(x = it.textX.toDp(), y = it.textY.toDp()),
+                text = it.text
+            )
         }
     }
 }
 
-@Composable
 internal fun ComposableDrawScope.drawBars(
     barLinePaths: List<BarPosition>,
     barLineBrush: Brush,
@@ -71,17 +70,16 @@ internal fun ComposableDrawScope.drawBars(
 
         // Text
         if (showFingerNumbers) {
-            drawComposable(
-                size = PositionalLayoutSize.WrapContent,
-                offset = Offset(x = it.textX, y = it.textY)
-            ) {
-                Text(text = it.text)
+            drawComposable {
+                Text(
+                    modifier = Modifier.offset(x = it.textX.toDp(), y = it.textY.toDp()),
+                    text = it.text
+                )
             }
         }
     }
 }
 
-@Composable
 internal fun ComposableDrawScope.drawNotes(
     notePositions: List<NotePosition>,
     noteSize: Float,
@@ -97,11 +95,11 @@ internal fun ComposableDrawScope.drawNotes(
         )
 
         if (showFingerNumbers) {
-            drawComposable(
-                size = PositionalLayoutSize.WrapContent,
-                offset = Offset(x = it.textX, y = it.textY)
-            ) {
-                Text(text = it.text)
+            drawComposable {
+                Text(
+                    modifier = Modifier.offset(x = it.textX.toDp(), y = it.textY.toDp()),
+                    text = it.text
+                )
             }
         }
     }
