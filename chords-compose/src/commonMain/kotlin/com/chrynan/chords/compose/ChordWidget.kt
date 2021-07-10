ef63544059f031cc4b9128c24b5e957f2ae6ddb2
Chris <ckeenan@starry.com>
2021-07-09 20:06:08 -0500

    Updated draw functions in ChordWidget for Jetpack Compose

--- chords-compose/src/commonMain/kotlin/com/chrynan/chords/compose/ChordWidget.kt
+++ chords-compose/src/commonMain/kotlin/com/chrynan/chords/compose/ChordWidget.kt
@@ -2,21 +2,13 @@
 
 package com.chrynan.chords.compose
 
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.layout.SubcomposeLayout
 import com.chrynan.chords.model.Chord
 import com.chrynan.chords.model.ChordChart
 import com.chrynan.chords.model.ChordViewModel
@@ -29,63 +21,58 @@ fun ChordWidget(
     chart: ChordChart = ChordChart.STANDARD_TUNING_GUITAR_CHART,
     viewModel: ChordViewModel = ChordViewModel()
 ) {
-    Text(modifier = modifier.offset(), text = "")
-    BoxWithConstraints {
-    }
-    Canvas(modifier = modifier) {
 
-    }
-    Layout(content = { }, modifier = Modifier.then()) { measurables, constraints ->
-        measurables.forEach { it.measure() }
-        measurables.forEach { it.parentData }
-        layout(0, 0) {
-
-        }
-    }
-    PositionalLayout {
-
-    }
 }
 
 @Composable
 private fun ComposableDrawScope.drawFretNumbers(
-    modifier: Modifier,
     fretNumberPoints: List<Offset>,
     chart: ChordChart,
-    fretLabelTextBrush: Brush,
     showFretNumbers: Boolean
 ) {
     // Fret numbers; check if we are showing them or not
     if (showFretNumbers) {
         fretNumberPoints.forEachIndexed { index, point ->
-            drawComposable(width = , height = , offset = ) {
-
+            drawComposable(
+                size = PositionalLayoutSize.WrapContent,
+                offset = Offset(x = point.x, y = point.y)
+            ) {
+                Text(text = (chart.fretStart.number + index).toString())
             }
-            // TODO drawText((chart.fretStart.number + index).toString(), point.x, point.y, fretLabelTextPaint)
         }
     }
 }
 
-private fun DrawScope.drawStringMarkers(
+@Composable
+private fun ComposableDrawScope.drawStringMarkers(
     stringTopMarkerPositions: List<StringPosition>,
     stringBottomLabelPositions: List<StringPosition>,
-    stringLabelTextBrush: Brush
 ) {
     // Top String markers (open/muted)
     stringTopMarkerPositions.forEach {
-        // TODO drawText(it.text, it.textX, it.textY, stringLabelTextPaint)
+        drawComposable(
+            size = PositionalLayoutSize.WrapContent,
+            offset = Offset(x = it.textX, y = it.textY)
+        ) {
+            Text(text = it.text)
+        }
     }
 
     // Bottom String labels (number/note)
     stringBottomLabelPositions.forEach {
-        // TODO drawText(it.text, it.textX, it.textY, stringLabelTextPaint)
+        drawComposable(
+            size = PositionalLayoutSize.WrapContent,
+            offset = Offset(x = it.textX, y = it.textY)
+        ) {
+            Text(text = it.text)
+        }
     }
 }
 
-private fun DrawScope.drawBars(
+@Composable
+private fun ComposableDrawScope.drawBars(
     barLinePaths: List<BarPosition>,
     barLineBrush: Brush,
-    noteLabelTextBrush: Brush,
     showFingerNumbers: Boolean
 ) {
     // Bars
@@ -100,16 +87,21 @@ private fun DrawScope.drawBars(
 
         // Text
         if (showFingerNumbers) {
-            // TODO canvas.drawText(it.text, it.textX, it.textY, noteLabelTextPaint)
+            drawComposable(
+                size = PositionalLayoutSize.WrapContent,
+                offset = Offset(x = it.textX, y = it.textY)
+            ) {
+                Text(text = it.text)
+            }
         }
     }
 }
 
-private fun DrawScope.drawNotes(
+@Composable
+private fun ComposableDrawScope.drawNotes(
     notePositions: List<NotePosition>,
     noteSize: Float,
     noteBrush: Brush,
-    noteLabelTextBrush: Brush,
     showFingerNumbers: Boolean
 ) {
     // Individual notes
@@ -121,7 +113,12 @@ private fun DrawScope.drawNotes(
         )
 
         if (showFingerNumbers) {
-            // TODO drawText(it.text, it.textX, it.textY, noteLabelTextBrush)
+            drawComposable(
+                size = PositionalLayoutSize.WrapContent,
+                offset = Offset(x = it.textX, y = it.textY)
+            ) {
+                Text(text = it.text)
+            }
         }
     }
 }
