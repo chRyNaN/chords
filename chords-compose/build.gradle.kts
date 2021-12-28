3745ac0297aa0e110b3ce53c8a40129db4d539c3
Christopher Keenan <ckeenan@starry.com>
2021-12-28 16:07:00 -0600

    Updated chords-compose build.gradle file to use Kotlin

---
+++ chords-compose/build.gradle.kts
@@ -0,0 +1,99 @@
+import com.chrynan.chords.buildSrc.LibraryConstants
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    id("maven-publish")
+    id("org.jetbrains.dokka")
+    id("org.jetbrains.compose")
+}
+
+group = LibraryConstants.group
+version = LibraryConstants.versionName
+
+kotlin {
+    android {
+        publishAllLibraryVariants()
+        publishLibraryVariantsGroupedByFlavor = true
+    }
+    targets {
+        android()
+        jvm()
+    }
+    sourceSets {
+        all {
+            languageSettings {
+                languageSettings.enableLanguageFeature("InlineClasses")
+            }
+        }
+        commonMain {
+            dependencies {
+                api(project(":chords-core"))
+
+                implementation(compose.runtime)
+                implementation(compose.ui)
+                implementation(compose.material)
+
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+
+                implementation("com.chrynan.colors:colors-jetpack-compose:0.7.0")
+            }
+        }
+    }
+}
+
+android {
+    compileSdk = LibraryConstants.Android.compileSdkVersion
+
+    defaultConfig {
+        minSdk = LibraryConstants.Android.minSdkVersion
+        targetSdk = LibraryConstants.Android.targetSdkVersion
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+            // Opt-in to experimental compose APIs
+            freeCompilerArgs = listOf(
+                    "-Xopt-in=kotlin.RequiresOptIn"
+            )
+        }
+    }
+
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    sourceSets["main"].java.srcDirs("src/androidMain/kotlin")
+    sourceSets["main"].res.srcDirs("src/androidMain/res")
+
+    sourceSets["test"].java.srcDirs("src/androidTest/kotlin")
+    sourceSets["test"].res.srcDirs("src/androidTest/res")
+}
+
+tasks.withType<Jar> { duplicatesStrategy = DuplicatesStrategy.INHERIT }
+
+afterEvaluate {
+    publishing {
+        repositories {
+            maven {
+                url = uri("https://repo.repsy.io/mvn/chrynan/public")
+
+                credentials {
+                    username = (project.findProperty("repsyUsername") ?: System.getenv("repsyUsername")) as? String
+                    password = (project.findProperty("repsyToken") ?: System.getenv("repsyToken")) as? String
+                }
+            }
+        }
+    }
+}
