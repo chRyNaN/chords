537e578daa6195a20e0208aae17d67b16408bb28
Christopher Keenan <ckeenan@starry.com>
2021-12-28 16:03:21 -0600

    Updated dependencies and converted some build files to use Kotlin

---
+++ chords-core/build.gradle.kts
@@ -0,0 +1,109 @@
+import com.chrynan.chords.buildSrc.LibraryConstants
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    id("kotlinx-serialization")
+    id("maven-publish")
+    id("org.jetbrains.dokka")
+}
+
+group = LibraryConstants.group
+version = LibraryConstants.versionName
+
+kotlin {
+    android {
+        publishAllLibraryVariants()
+        publishLibraryVariantsGroupedByFlavor = true
+    }
+    targets {
+        android()
+        jvm()
+        js(BOTH) {
+            browser()
+            nodejs()
+        }
+        ios()
+    }
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation("org.jetbrains.kotlin:kotlin-stdlib-common")
+
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+
+                api("com.chrynan.colors:colors-core:0.7.0")
+            }
+        }
+        all {
+            languageSettings.enableLanguageFeature("InlineClasses")
+            languageSettings.useExperimentalAnnotation("kotlin.RequiresOptIn")
+        }
+    }
+}
+
+android {
+    compileSdk = LibraryConstants.Android.compileSdkVersion
+
+    defaultConfig {
+        minSdk = LibraryConstants.Android.minSdkVersion
+        targetSdk = LibraryConstants.Android.targetSdkVersion
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+            // Opt-in to experimental compose APIs
+            freeCompilerArgs = listOf(
+                    "-Xopt-in=kotlin.RequiresOptIn"
+            )
+        }
+    }
+
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    sourceSets["main"].java.srcDirs("src/androidMain/kotlin")
+    sourceSets["main"].res.srcDirs("src/androidMain/res")
+
+    sourceSets["test"].java.srcDirs("src/androidTest/kotlin")
+    sourceSets["test"].res.srcDirs("src/androidTest/res")
+}
+
+tasks.withType<Jar> { duplicatesStrategy = DuplicatesStrategy.INHERIT }
+
+dependencies {
+    implementation("androidx.annotation:annotation:1.3.0")
+
+    implementation("androidx.appcompat:appcompat:1.4.0")
+
+    implementation("com.chrynan.parcelable:parcelable-core:0.3.1")
+    implementation("com.chrynan.parcelable:parcelable-android:0.3.1")
+}
+
+afterEvaluate {
+    publishing {
+        repositories {
+            maven {
+                url = uri("https://repo.repsy.io/mvn/chrynan/public")
+
+                credentials {
+                    username = (project.findProperty("repsyUsername") ?: System.getenv("repsyUsername")) as? String
+                    password = (project.findProperty("repsyToken") ?: System.getenv("repsyToken")) as? String
+                }
+            }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "chords"
+
+include(":chords-core")
+include(":chords-compose")
+// include ":sample-android"
+include(":sample-js")
+include(":sample-compose")
