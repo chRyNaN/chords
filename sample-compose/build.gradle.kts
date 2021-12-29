a71197446f7b6b79a2ac4d9b230f1c4bf82c58f4
Christopher Keenan <ckeenan@starry.com>
2021-12-28 23:05:12 -0600

    Reenabled chords-compose and sample-compose modules

---
+++ sample-compose/build.gradle.kts
@@ -0,0 +1,83 @@
+import com.chrynan.chords.buildSrc.LibraryConstants
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("multiplatform")
+    id("com.android.application")
+    id("org.jetbrains.compose")
+}
+
+group = LibraryConstants.group
+version = LibraryConstants.versionName
+
+kotlin {
+    targets {
+        android()
+        jvm()
+    }
+    sourceSets {
+        all {
+            languageSettings {
+                languageSettings.enableLanguageFeature("InlineClasses")
+            }
+        }
+        commonMain {
+            dependencies {
+                api(project(":chords-compose"))
+
+                implementation(compose.runtime)
+                implementation(compose.ui)
+                implementation(compose.material)
+            }
+        }
+    }
+}
+
+android {
+    compileSdk = LibraryConstants.Android.compileSdkVersion
+
+    defaultConfig {
+        applicationId = "com.chrynan.chords.sample.compose"
+        minSdk = LibraryConstants.Android.minSdkVersion
+        targetSdk = LibraryConstants.Android.targetSdkVersion
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+            // Opt-in to experimental compose APIs
+            freeCompilerArgs = listOf(
+                    "-Xopt-in=kotlin.RequiresOptIn"
+            )
+        }
+    }
+
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    sourceSets["main"].java.srcDirs("src/androidMain/kotlin")
+    sourceSets["main"].res.srcDirs("src/androidMain/res")
+
+    sourceSets["test"].java.srcDirs("src/androidTest/kotlin")
+    sourceSets["test"].res.srcDirs("src/androidTest/res")
+}
+
+tasks.withType<Jar> { duplicatesStrategy = DuplicatesStrategy.INHERIT }
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.4.0")
+    implementation("com.google.android.material:material:1.4.0")
+
+    implementation("androidx.activity:activity-compose:1.4.0")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "chords"
 
 include(":chords-core")
-// include(":chords-compose")
-include(":sample-android")
+include(":chords-compose")
+// include(":sample-android")
 include(":sample-js")
-// include(":sample-compose")
+include(":sample-compose")
