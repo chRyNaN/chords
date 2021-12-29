9903642c9a5a284cb742b62f5db65f6eeb318a9a
Christopher Keenan <ckeenan@starry.com>
2021-12-29 16:45:29 -0600

    Bumped version to 2.4.1 (6)

--- buildSrc/src/main/java/com.chrynan.chords.buildSrc/LibraryConstants.kt
+++ buildSrc/src/main/java/com.chrynan.chords.buildSrc/LibraryConstants.kt
@@ -1,14 +1,15 @@
+@file:Suppress("MemberVisibilityCanBePrivate")
+
 package com.chrynan.chords.buildSrc
 
 object LibraryConstants {
 
-    const val bintrayRepo = "chrynan"
     const val group = "com.chrynan.chords"
     const val owner = "chrynan"
     const val repoName = "chords"
-    const val versionName = "2.4.0"
-    const val versionCode = 5
-    const val versionDescription = "Release 2.4.0"
+    const val versionName = "2.4.1"
+    const val versionCode = 6
+    const val versionDescription = "Release $versionName ($versionCode)"
     const val license = "Apache-2.0"
     const val vcsUrl = "https://github.com/chRyNaN/chords.git"
 
